e606910c9de0924ee6ef32a133c657d6ac785d75
fcerfon <florent.cerfon@gmail.com>
2018-12-12 10:22:03 +0100

    creation du pub client

--- cli/index.js
+++
@@ -1,8 +0,0 @@
-var pubServices = require('../services/pubs.service');
-
-var openPubs = pubServices.getOpenPubs();
-console.log(openPubs);
-
-services: {
-    pubServices : pubServices
-}
---
+++ pub-services/index.js
@@ -0,0 +1,5 @@
+var pubServices = require('./services/pubs.service');
+
+services: {
+    pubServices : pubServices
+}
